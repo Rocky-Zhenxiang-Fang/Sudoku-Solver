be02d67ebfcc9088824e5de4f5ec12227c491fbc
Rocky Zhenxiang Fang <rockys910109@gmail.com>
2020-09-11 15:18:00 +0800

    Able to display highlighed box and used and unused number

--- GUI.py
+++ GUI.py
@@ -2,17 +2,21 @@ import pygame
 from Sudoku_GUI import SudokuGUI
 import sys
 
-BOX_LENGTH = 70
+BOX_LENGTH: int = 70
 LINE_THIN = 5
 LINE_THICK = 8
 WINDOW_WIDTH = BOX_LENGTH * 9 + 100
-WINDOW_HEIGHT = WINDOW_WIDTH + 140
+WINDOW_HEIGHT = WINDOW_WIDTH + 100
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 
 def draw_sudoku(sc, sudo: SudokuGUI):
     font = pygame.font.Font(None, 60)
+    # Draw numbers
     for r in range(9):
         for c in range(9):
             if sudo.origin[r][c] == 0:
@@ -21,6 +25,19 @@ def draw_sudoku(sc, sudo: SudokuGUI):
             num_rect = num.get_rect()
             num_rect.center = (50 + BOX_LENGTH * (c + 0.5), BOX_LENGTH * (r + 0.5) + 70)
             sc.blit(num, num_rect)
+
+
+def draw_status(sc, sudo: SudokuGUI, c, r):
+    font = pygame.font.Font(None, 40)
+    unchecked = font.render("Unchecked = " + str(sudo.boxes[r][c].unchecked), False, BLUE)
+    un_rect = unchecked.get_rect()
+    un_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 90)
+    sc.blit(unchecked, un_rect)
+    checked = font.render("Checked = " + str(sudo.boxes[r][c].checked), False, RED)
+    ch_rect = unchecked.get_rect()
+    ch_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT - 45)
+    sc.blit(checked, ch_rect)
+
 
 def main(board):
     pygame.init()
@@ -35,10 +52,10 @@ def main(board):
     w = 0
     while w < 10:
         if w % 3 == 0:
-            pygame.draw.line(bg, BLACK, (w * BOX_LENGTH + 50, 70), (w * BOX_LENGTH + 50, WINDOW_HEIGHT - 170),
+            pygame.draw.line(bg, BLACK, (w * BOX_LENGTH + 50, 70), (w * BOX_LENGTH + 50, WINDOW_HEIGHT - 130),
                              LINE_THICK)
         else:
-            pygame.draw.line(bg, BLACK, (w * BOX_LENGTH + 50, 70), (w * BOX_LENGTH + 50, WINDOW_HEIGHT - 170),
+            pygame.draw.line(bg, BLACK, (w * BOX_LENGTH + 50, 70), (w * BOX_LENGTH + 50, WINDOW_HEIGHT - 130),
                              LINE_THIN)
         w += 1
 
@@ -52,20 +69,28 @@ def main(board):
                              LINE_THIN)
         h += 1
     screen.blit(bg, (0, 0))
-
     sudoku = SudokuGUI(board)
-
     draw_sudoku(screen, sudoku)
-
-
-
     pygame.display.update()
 
     running = True
+    r, c = None, None
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                if r and c:
+                    pygame.draw.rect(bg, BLACK, (50 + c * BOX_LENGTH, 70 + r * BOX_LENGTH, BOX_LENGTH, BOX_LENGTH), LINE_THIN)
+                pos = pygame.mouse.get_pos()
+                c = (pos[0] - 50) // BOX_LENGTH
+                r = (pos[1] - 70) // BOX_LENGTH
+                pygame.draw.rect(bg, GREEN, (50 + c * BOX_LENGTH, 70 + r * BOX_LENGTH, BOX_LENGTH, BOX_LENGTH), LINE_THIN)
+                screen.blit(bg, (0, 0))
+                draw_status(screen, sudoku, c, r)
+                draw_sudoku(screen, sudoku)
+                pygame.display.update()
+
     pygame.quit()
 
 
